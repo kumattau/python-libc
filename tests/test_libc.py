1acc61f9169f49134d95f550bc9770fa14eb4ece
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-02-12 09:52:57 +0900

    add handle close

--- tests/test_libc.py
+++ tests/test_libc.py
@@ -1,4 +1,5 @@
 import time
+import pytest
 
 from libc import *
 
@@ -17,3 +18,10 @@ def test_timerfd():
     _ = os.read(tfd, 8)
     t = time.perf_counter() - t
     assert 2 - 1e3 < t < 2 + 1e3
+
+    # close timerfd
+    os.close(tfd)
+
+    # try to close the timerfd which was already closed.
+    with pytest.raises(OSError):
+        os.close(tfd)
