bf3b59ab4a737b6a8b37b2215dee6788147ffef8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-02-27 23:36:34 +0900

    add timerfd_settime_ns/timerfd_gettime_ns (#5)

--- tests/test_libc.py
+++ tests/test_libc.py
@@ -13,15 +13,38 @@ def test_getpid():
     assert pid == os.getpid()
 
 
-def test_timerfd():
+@pytest.mark.parametrize("interval, value, count", [
+    ( 0.5  , 1, 3 ),
+    ( 0.125, 0.25, 2 ),
+])
+def test_timerfd(interval: float, value: float, count: int):
+    # acceptable error is 1 msec or less.
+    limit_error = 2e-3
+
     tfd = timerfd_create(CLOCK.REALTIME, 0)
-    timerfd_settime(tfd, 0, (0.5, 1))
+
+    # 1st call of timerfd_settime
+    interval2, value2 = timerfd_settime(tfd, 0, (interval, value))
+    assert(interval2 == 0)
+    assert(value2 == 0)
+
+    # 2nd call of timerfd_settime
+    interval2, value2 = timerfd_settime(tfd, 0, (interval, value))
+    assert(interval2 == interval)
+    assert(abs(value2 - value) < limit_error)
+
+    # check by timerfd_gettime
+    interval2, value2 = timerfd_gettime(tfd)
+    assert(interval2 == interval)
+    assert(abs(value2 - value) < limit_error)
+
     t = time.perf_counter()
-    _ = os.read(tfd, 8)
-    _ = os.read(tfd, 8)
-    _ = os.read(tfd, 8)
+    for _ in range(count):
+        _ = os.read(tfd, 8)
     t = time.perf_counter() - t
-    assert 2 - 1e-3 < t < 2 + 1e-3
+
+    total_time = value +  interval * (count - 1)
+    assert(abs(t - total_time) < limit_error)
 
     # close timerfd
     os.close(tfd)
@@ -34,6 +57,57 @@ def test_timerfd():
     assert exc_info.value.args[0] == errno.EBADF
     assert exc_info.value.args[1] == os.strerror(errno.EBADF)
 
+
+@pytest.mark.parametrize("interval, value, count", [
+    ( SEC_IN_NS //   2, SEC_IN_NS     , 3 ),
+    ( SEC_IN_NS //  10, SEC_IN_NS //20, 2 ),
+])
+def test_timerfd_ns(interval: int, value: int, count: int):
+    # acceptable error is 1 msec or less.
+    limit_error = SEC_IN_NS // 1000
+
+    # check availability of `time.perf_counter_ns()`.
+    # If it is not available (expected on python 3.6), define a stub.
+    try:
+        _ = time.perf_counter_ns()
+    except AttributeError:
+        time.perf_counter_ns = lambda: int(time.perf_counter() * SEC_IN_NS)
+
+    tfd = timerfd_create(CLOCK.REALTIME, 0)
+
+    # 1st call of timerfd_settime_ns
+    interval2, value2 = timerfd_settime_ns(tfd, 0, (interval, value))
+    assert(interval2 == 0)
+    assert(value2 == 0)
+
+    # 2nd call of timerfd_settime_ns
+    interval2, value2 = timerfd_settime_ns(tfd, 0, (interval, value))
+    assert(interval2 == interval)
+    assert(abs(value2 - value) < limit_error)
+
+    # check by timerfd_gettime_ns
+    interval2, value2 = timerfd_gettime_ns(tfd)
+    assert(interval2 == interval)
+    assert(abs(value2 - value) < limit_error)
+
+    t = time.perf_counter_ns()
+    for _ in range(count):
+        _ = os.read(tfd, 8)
+    t = time.perf_counter_ns() - t
+
+    total_time = value +  interval * (count - 1)
+    assert(abs(t - total_time) < limit_error)
+
+    # close timerfd
+    os.close(tfd)
+
+    # try to close the timerfd which was already closed.
+    with pytest.raises(OSError) as exc_info:
+        os.close(tfd)
+
+    # check detail of OSError
+    assert exc_info.value.args[0] == errno.EBADF
+    assert exc_info.value.args[1] == os.strerror(errno.EBADF)
 
 def test_eventfd():
     efd = eventfd(0, 0)
